99975b096a2b5be4fe6d3ee870ada7a3a1992230
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-01-30 18:48:27 -0700

    Add card class

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+class Card {
+    constructor(number, question){
+    this.question = question
+    this.answers = ['object', 'array', 'function']
+    this.correctAnswer = 'object'
+    }
+}
+
+
+
+module.exports = Card
