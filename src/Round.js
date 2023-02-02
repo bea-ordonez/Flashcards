ef9f89a782625fee90d731a3c23c7a0977cf01a3
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-02-01 21:47:19 -0700

    Add round class

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,37 @@
 class Round{
-    constructor(){
-   
+    constructor(deck){
+        this.turns = 0;
+        this.deck = deck.cards;
+        this.cardPosition = 0;
+        this.currentCard = this.deck[this.cardPosition];
+        this.incorrectGuesses = [];
+        this.currentTurn = null;
+    }
+
+    returnCurrentCard() {
+        return this.currentCard
+    }
+
+    takeTurn(guess) {
+        this.currentTurn = new Turn(guess, this.currentCard);
+        this.turns += 1;
+        this.cardPosition += 1;
+        this.currentCard = this.deck[this.cardPosition];
+        this.currentTurn.evaluateGuess();
+        if(!this.currentTurn.correct) {
+            this.incorrectGuesses.push(this.currentCard.id);
+        }
+        return this.currentTurn.giveFeedback();
+    }
+
+    calculatePercentCorrect() {
+        let correctGuesses = this.turns - this.incorrectGuesses.length;
+        let percentCorrect = correctGuesses / this.turns * 100;
+        return percentCorrect;
+    }
+
+    endRound() {
+        return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`;
     }
 }
 
